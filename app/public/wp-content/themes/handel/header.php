77ac43cdef965634b90e43bbff32fb55b93eff6f
Victor Borges <borgesvictormachado@gmail.com>
2022-11-10 22:27:59 -0300

    add header part 1

--- app/public/wp-content/themes/handel/header.php
+++ app/public/wp-content/themes/handel/header.php
@@ -1,10 +1,30 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo('name') ?> | <?php the_title(); ?></title>
-    <?php wp_head(); ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><?php bloginfo('name') ?> | <?php wp_title(); ?></title>
+  <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?> >
+<body <?php body_class(); ?>>
+
+<?php
+
+    $img_url = get_stylesheet_directory_uri() . '/img';
+
+?>
+
+<header class="header">
+    <a href=""> 
+        <img src="<?= $img_url ?>/handel.svg" alt="Handel" />
+    </a>
+
+    <div class="search-class">
+        <form action="<?php bloginfo('url'); ?>/loja/" method="get">
+            <input type="text" name="s" id="s" placeholder="Search" value="<?php the_search_query(); ?>" />
+            <input type="text" name="post_type" value="product" class="hidden" />
+            <input type="submit" id="searchbutton" value="Search" />
+        </form>
+    </div>
+</header>
--- app/public/wp-content/themes/handel/page-home.php
+++ app/public/wp-content/themes/handel/page-home.php
@@ -1,24 +1,10 @@
-<?php 
-    // Template name: Home
-
-    get_header(); 
-
-?>
-
 <?php
+// Template name: Home
+get_header(); ?>
 
-    if(have_posts()) :
-        while(have_posts()) :
-            the_post();
-?>
-
-<h1><?php the_title(); ?></h1>
-<main><?php the_content(); ?></main>
-<?php
-        
-        endwhile;
-    endif;
-
-?>
+<?php if(have_posts()) { while (have_posts()) { the_post(); ?>
+  <h1><?php the_title(); ?></h1>
+  <main><?php the_content(); ?></main>
+<?php } } ?>
 
 <?php get_footer(); ?>
