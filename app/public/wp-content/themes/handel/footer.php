8781c3586fd32fcfa45fb83a361d88f683344670
Victor Borges <borgesvictormachado@gmail.com>
2022-11-20 17:33:10 -0300

    add slide and styled

--- app/public/wp-content/themes/handel/footer.php
+++ app/public/wp-content/themes/handel/footer.php
@@ -1,4 +1,6 @@
 
 <?php wp_footer(); ?>
+<script src="<?= get_stylesheet_directory_uri(); ?>/js/slide.js"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/js/script.js"></script>
 </body>
 </html>
--- app/public/wp-content/themes/handel/page-home.php
+++ app/public/wp-content/themes/handel/page-home.php
@@ -1,10 +1,67 @@
 <?php
 // Template name: Home
 get_header(); ?>
+<pre>
+<?php
+
+$products_slide = wc_get_products([
+  'limit' => 6,
+  'tag' => ['slide'],
+]);
+
+
+function format_products($products) {
+  $products_final = [];
+
+  foreach($products as $product) {
+    $products_final[] = [
+      'name'=> $product->get_name(),
+      'preco' => $product->get_price_html(),
+      'link' => $product->get_permalink(),
+      'img' => wp_get_attachment_image_src($product->get_image_id(), 'slide')[0],
+    ];
+  }
+  return $products_final;
+}
+
+$slide = format_products($products_slide);
+
+
+?>
+</pre>
+
 
 <?php if(have_posts()) { while (have_posts()) { the_post(); ?>
-  <h1><?php the_title(); ?></h1>
-  <main><?php the_content(); ?></main>
+
+  <ul class="vantagens">
+    <li>Frete Grátis</li>
+    <li>Troca Fácil</li>
+    <li>Até 12x</li>
+  </ul>
+
+  <section class="slide-wrapper">
+
+   <ul class="slide">
+
+    <?php foreach($slide as $product) : ?>
+
+    <li class="slide-item">
+      <img src="<?= $product['img'] ?>" alt="<?= $product['name'] ?>" />
+
+      <div class="slide-info">
+        <span class="slide-preco"><?= $product['preco'] ?></span>
+        <h2 class="slide-nome"><?= $product['name'] ?></h2>
+        <a class="slide-link" href="<?= $product['link'] ?>">Ver Mais</a>
+      </div>
+    
+    </li>
+
+    <?php endforeach ?>
+
+   </ul> 
+
+  </section>
+
 <?php } } ?>
 
 <?php get_footer(); ?>
