59a544612248fa0be78d71d1b259303f476faa87
Victor Borges <borgesvictormachado@gmail.com>
2022-11-21 19:08:04 -0300

    add section category

--- app/public/wp-content/themes/handel/page-home.php
+++ app/public/wp-content/themes/handel/page-home.php
@@ -44,6 +44,27 @@ $data['slide'] = format_products($products_slide, 'slide');
 $data['lancamentos'] = format_products($products_new, 'medium');
 $data['vendidos'] = format_products($products_sales, 'medium');
 
+
+$home_id = get_the_ID();
+$categoria_esquerda = get_post_meta($home_id, 'categoria_esquerda', true);
+$categoria_direita = get_post_meta($home_id, 'categoria_direita', true);
+
+function get_product_category_data($category) {
+  $cat = get_term_by('slug', $category, 'product_cat');
+  $cat_id = $cat->term_id;
+  $img_id = get_term_meta($cat_id, 'thumbnail_id', true);
+
+  return [
+    'id' => $cat_id,
+    'name' => $cat->name,
+    'link' => get_term_link($cat_id, 'product_cat'),
+    'img' => wp_get_attachment_image_src($img_id, 'slide')[0],
+  ];
+}
+
+$data['categorias'][$categoria_esquerda] = get_product_category_data($categoria_esquerda);
+$data['categorias'][$categoria_direita] = get_product_category_data($categoria_direita);
+
 ?>
 
 
@@ -81,6 +102,15 @@ $data['vendidos'] = format_products($products_sales, 'medium');
   <section class="container">
     <h1 class="subtitle">Lançamentos</h1>
     <?php handel_product_list($data['lancamentos']); ?>
+  </section>
+
+  <section class="categorias-home">
+      <?php foreach($data['categorias'] as $categoria) : ?>
+        <a href="<?= $categoria['link'] ?>">
+          <img src="<?= $categoria['img'] ?>" alt="<?= $categoria['name'] ?>">
+          <span class="btn-link"><?= $categoria['name'] ?></span>
+        </a>
+      <?php endforeach ?>
   </section>
 
   <section class="container">
